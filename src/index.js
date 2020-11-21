49d95eacec75c72a768a051734de355db38a2101
swagatika1101 <47102508+swagatika1101@users.noreply.github.com>
2020-11-21 18:34:34 +0530

    Added inline styling and if and else statement

--- src/index.js
+++ src/index.js
@@ -1,21 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//Dynamically change the color using inline css styles
+const customStyle = {
+  color: "Black",
+  fontSize: "30px"
+};
+
+//end
+
+//To change greeting acc to time
+const date = new Date();
+const currentTime = date.getHours();
+
+let wish;
+
+if (currentTime < 12) {
+  wish = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 16) {
+  wish = "Good Afternoon";
+  customStyle.color = "green";
+} else if (currentTime < 20) {
+  wish = "Good Evening";
+  customStyle.color = "blue";
+} else {
+  wish = "Good Night";
+  customStyle.color = "brown";
+}
+
+//end greeting section
+
+//to change year in footer
 const Aname = "P Swagatika";
 const currentDate = new Date();
 const year = currentDate.getFullYear();
-const img = "https://picsum.photos/500/250";
-const customStyle = {
-  color: "Black",
-  fontSize: "30px",
-  border: "1px solid black"
-};
 
-customStyle.color = "grey";
+//end
+
+//to add img constant in render function
+const img = "https://picsum.photos/500/250";
+//end
 
 ReactDOM.render(
   <div>
-    <h1 className="mainHeading">My Posters</h1>
+    <h1 className="Heading" style={customStyle}>
+      {wish}
+    </h1>
     <div>
       <img alt="biriyani" src={img} />
     </div>
